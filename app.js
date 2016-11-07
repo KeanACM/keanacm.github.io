7333f337b78d002f3c4d558af9e0d513ec7decb6
Jonathan Rodriguez <jonathanrodriguez224@gmail.com>
2016-11-07 16:49:08 -0500

    refactor in angular, lets see if it works

---
+++ app.js
@@ -0,0 +1,26 @@
+var App = angular.module('App', ['ui.router']);
+
+App.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+  
+    .state('/home', {
+        url: '/home',
+        templateUrl: 'home/index.html'
+    })
+    .state('/about', {
+        url: '/about',
+        templateUrl: 'about/index.html'
+    })
+    .state('/resources', {
+        url: '/resources',
+        templateUrl: 'resources/index.html'
+    })
+        
+});
+
+App.controller('main', function($scope) {
+    
+});
