e5d7ed073661a2fd5d9b3d517140cc21e3a9f24c
Jonathan Rodriguez <jonathanrodriguez224@gmail.com>
2016-11-07 17:48:22 -0500

    broke more things than i expected

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 var App = angular.module('App', ['ui.router']);
 
-App.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
-    
+App.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
     
     $stateProvider
   
-    .state('/home', {
+    .state('home', {
         url: '/home',
         templateUrl: 'home/index.html'
     })
-    .state('/about', {
+    .state('about', {
         url: '/about',
         templateUrl: 'about/index.html'
     })
-    .state('/resources', {
+    .state('resources', {
         url: '/resources',
         templateUrl: 'resources/index.html'
     });
-    
-    $locationProvider.html5Mode(true);
-        
 });
 
 App.controller('main', function($scope) {
